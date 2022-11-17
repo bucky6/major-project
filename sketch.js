8237e9c9c3dcb744688cebd50dff467bba1b8f83
bucky6 <114016486+bucky6@users.noreply.github.com>
2022-11-17 12:02:55 -0600

    initial commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// Project Title
+// Bucky Pederson-Bradbury
+// November 17, 2022
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
