b4a2d3489bbc9a873594ad61ff8a20e4933ba648
bucky6 <114016486+bucky6@users.noreply.github.com>
2022-12-01 12:17:27 -0600

    continued intro screens and rethought format of game

--- sketch.js
+++ sketch.js
@@ -6,12 +6,11 @@
 // - describe what you did to take this project "above and beyond"
 
 let state = "mainMenu";
-let backgroundColor = 220;
-let nightCastleImage, titleTextImage;
+let backgroundColor = 25;
+let nightCastleImage;
 
 function preload() {
   nightCastleImage = loadImage("assets/nightCastle.png");
-  titleTextImage = loadImage("assets/titleText.png");
 }
 
 function setup() {
@@ -19,39 +18,47 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(25);
   if (state === "mainMenu") {
     mainMenu();
   }
   if (state === "intro") {
     introScreen();
   }
+  if (state === "castle") {
+    image(nightCastleImage, 0, 0, width, height);
+  }
+  if (state === "game") {
+    background(220);
+  }
 }
 
 // make the main menu
 function mainMenu() {
   background(backgroundColor);
-  textFont("Dungeon Regular", 50);
+  // fill("white");
+  textFont("georgia", 50);
   text("Escape From The Castle", width/2 - 240, height/3);
   text("Play", width/2 - 100, height/2);
+  rect(width/2 - 125, height/2 - 75, 250, 150);
   if (state === "mainMenu" && mouseInsideRect(width/2 - 125, width/2 + 125, height/2 - 75, height/2 + 75)) {
     fill("grey");
   }
   else {
     fill("white");
   }
-  rect(width/2 - 125, height/2 - 75, 250, 150);
 }
 
 //make the game screen
 function introScreen() {
   background (0);
-  text("You are a real estate agent. You've been called away to a strange castle in the country to speak with the owner.", width/2 - 700, height/3);
-  text("After a long day's journey, you have arrived.", width/2 - 300, height/3 + 75);
-  text("Press space to continue.", width/2 - 175, height/3 + 150);
-  textFont("georgia", 30);
-  rect(width/2 - 75, height/2 + 100, 150, 75);
   fill("white");
+  text("You are a real estate agent. You've been called away to a strange castle in the country to speak with the owner.", width/2 - 700, height/3);
+  text("Despite the many unnerving things you've heard about this castle and it resident(s), money is money.", width/2 - 650, height/3 + 75);
+  text("After a long day's journey, you have arrived.", width/2 - 300, height/3 + 150);
+  text("Press space to continue", width/2 - 150, height/3 + 450);
+  textFont("georgia", 10);
+  textFont("georgia", 30);
 }
 
 function mouseInsideRect(left, right, top, bottom) {
@@ -63,9 +70,15 @@ function mousePressed() {
   if (state === "mainMenu" && mouseInsideRect(width/2 - 125, width/2 + 125, height/2 - 75, height/2 + 75)) {
     state = "intro";
   }
-  if (state === "intro" && mouseInsideRect(width/2 - 150, width/2 +150, height/2 +500, height/2 + 650)) {
-    image(nightCastleImage, 0, 0, width, height);
+}
+
+function keyPressed() {
+  if (state === "intro" && keyCode === 32) {
+    state = "castle";
   }
+  // if (state === "castle" && keyCode === 32) {
+  //   state = "game";
+  // }
 }
 
 // To Do (somewhat in order?)
